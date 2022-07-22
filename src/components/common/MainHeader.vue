a48d6f886dbe41ee0eef4999d7db7eb95eedb15b
Leepilung <ek3434@naver.com>
2022-07-22 16:25:29 +0900

    feat: Header 퍼블리싱

--- src/components/common/MainHeader.vue
+++ src/components/common/MainHeader.vue
@@ -1,7 +1,54 @@
-<template lang="">
-  <div>MainHeader</div>
+<template>
+  <header>
+    <div class="header">
+      <div class="header__logo"></div>
+      <div class="header__alramAndProfile">
+        <div class="header__alarm"></div>
+        <div class="header__profile"></div>
+      </div>
+    </div>
+  </header>
 </template>
 <script>
 export default {};
 </script>
-<style lang=""></style>
+<style lang="scss" scoped>
+header {
+  width: 100%;
+  background: var(--color-white);
+}
+.header {
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: var(--color-white);
+  margin: 0.813rem 1.875rem 0.813rem 1.688rem;
+
+  &__logo {
+    width: 40px;
+    height: 40px;
+    background: url('@/assets/img_flitto.png') no-repeat;
+    background-size: cover;
+    border-radius: 10px;
+  }
+  &__alramAndProfile {
+    display: flex;
+    background: inherit;
+  }
+  &__alarm {
+    width: 25px;
+    height: 28px;
+    margin-right: 1.375rem;
+    background: url('@/assets/svg/ic_alarm.svg') no-repeat;
+    background-size: cover;
+  }
+  &__profile {
+    width: 30px;
+    height: 30px;
+    background: url('@/assets/img_flitto.png') no-repeat;
+    background-size: cover;
+    border-radius: 50%;
+  }
+}
+</style>
