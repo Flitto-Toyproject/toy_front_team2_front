61eb087a53cfed7f786902600933e240f5490ed5
Leepilung <ek3434@naver.com>
2022-08-02 11:45:16 +0900

    feat: 라우팅 확인용 임시 구문 추가

--- src/components/calendar/MainCalendar.vue
+++ src/components/calendar/MainCalendar.vue
@@ -1,7 +1,20 @@
-<template lang="">
-  <div>MainCalendar PAge</div>
+<template>
+  <div class="test">
+    <h1>테스트용 캘린더 페이지</h1>
+  </div>
 </template>
 <script>
 export default {}
 </script>
-<style lang=""></style>
+<style lang="scss" scoped>
+.test {
+  border: 3px solid blue;
+  width: 300px;
+  height: 300px;
+  background: inherit;
+
+  h1 {
+    background: inherit;
+  }
+}
+</style>
