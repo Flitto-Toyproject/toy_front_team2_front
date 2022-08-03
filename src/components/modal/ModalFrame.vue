1b807ce9a5eef3242533efed5db8f5b9970371cb
Leepilung <ek3434@naver.com>
2022-08-03 19:03:43 +0900

    feat: 모달 프레임 추가

---
+++ src/components/modal/ModalFrame.vue
@@ -0,0 +1,58 @@
+<template>
+  <transition name="modal" appear>
+    <section class="modal-background" @click.self="$emit('close')">
+      <article class="modal-frame">
+        <slot />
+      </article>
+    </section>
+  </transition>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-enter-active,
+.modal-leave-active {
+  transition: opacity 0.6s ease;
+}
+
+.modal-enter-from,
+.modal-leave-to {
+  opacity: 0;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.modal-background {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  padding: 20px;
+  align-items: center;
+  z-index: 99;
+}
+
+.modal-frame {
+  margin: auto;
+  width: 410px;
+  height: 400px;
+  background: var(--color-white);
+  border-radius: 10px;
+}
+</style>
