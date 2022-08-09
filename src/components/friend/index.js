d32b7e6a185925e8b4736e7938c188bfa62c868e
sohyeonkim <sohyeon.kim@flitto.com>
2022-08-09 19:07:39 +0900

    feat: 친구찾기 페이지 UI 뼈대 작성

---
+++ src/components/friend/index.js
@@ -0,0 +1,5 @@
+import SearchBox from './SearchBox.vue'
+import FriendCard from './FriendCard.vue'
+import PaginationItem from './PaginationItem.vue'
+
+export { SearchBox, FriendCard, PaginationItem }
