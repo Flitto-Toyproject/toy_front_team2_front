f6447178aeb4e3b5539c651fbf56c48bec4feb15
sohyeonkim <sohyeon.kim@flitto.com>
2022-08-10 19:12:52 +0900

    feat: MainFriend에서 import한 컴포넌트들 누락되어서 추가

---
+++ src/components/friend/PaginationItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <article class="friend-pagination">
+    <button class="friend-pagination__prev">이전</button>
+    <button class="friend-pagination__number">1</button>
+    <button class="friend-pagination__number">2</button>
+    <button class="friend-pagination__number">3</button>
+    <button class="friend-pagination__next">다음</button>
+  </article>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+.froemd-pagination {
+}
+
+.froemd-pagination-prev {
+}
+
+.froemd-pagination-number {
+}
+
+.froemd-pagination-next {
+}
+</style>
---
+++ src/components/friend/SearchBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="friend-header__search">
+    <div class="friend-header__search-wrapper">
+      <i class="friend-header__search-icon"></i>
+      <input
+        class="friend-header__search-input"
+        v-model="search_value"
+        placeholder="이름, 아이디로 검색"
+      />
+    </div>
+    <i class="friend-header__search-cancel-icon"></i>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      search_value: '',
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.friend-header__search {
+}
+
+.friend-header__search-wrapper {
+}
+
+.friend-header__search-icon {
+}
+
+.friend-header__search-input {
+}
+
+.friend-header__search-cancel-icon {
+}
+</style>
