ed5df8ff1cef318f0e98da4a838e1bbf558bd2ef
Leepilung <ek3434@naver.com>
2022-08-11 12:35:43 +0900

    feat: 알람 페이지 퍼블리싱(스크립트 제외)

--- src/components/alarm/MainAlaram.vue
+++ src/components/alarm/MainAlaram.vue
@@ -1,20 +1,237 @@
 <template>
-  <div class="test">
-    <h1>테스트용 알람 페이지</h1>
-  </div>
+  <section class="alarm">
+    <article class="alarm__header">
+      <p class="alarm__header-title"># 전체 알림</p>
+      <div class="alarm__header-menu">
+        <div class="alarm__header-menubox">
+          <span class="menubox-all">전체</span> |
+          <span class="menubox-friend">친구</span> |
+          <span class="menubox-schedule">일정</span>
+        </div>
+        <div class="menubox-read-all">알림 전체 읽기</div>
+      </div>
+      <!-- 바디 부분 -->
+      <article class="alarm__body">
+        <div
+          v-for="(schedule, i) in schedules"
+          :key="i"
+          class="alarm__body-title"
+        >
+          <hr />
+          <div class="alarm__body-date">{{ schedule.date }}</div>
+          <!-- 카드 부분 -->
+          <div
+            class="alarm__body-card-box"
+            :class="{ 'alarm__body-card-box-clicked': !task.read }"
+            v-for="(task, i) in schedule.body"
+            :key="i"
+          >
+            <!-- 아이콘 및 프로필 -->
+            <i class="alarm__body-card-icon" />
+            <img class="alarm__body-card-porifle" :src="task.porfileUrl" />
+            <!-- 친구 추가일 경우 -->
+            <div class="alarm__body-card-content" v-if="task.type === '친구'">
+              <p>{{ task.from }}님이 회원님을 친구 추가 하였습니다.</p>
+              <p class="card-content-announce">
+                {{ task.type }} <span>| {{ '오늘' }}</span>
+              </p>
+            </div>
+            <!-- 일정 추가일 경우 -->
+            <div class="alarm__body-card-content" v-if="task.type === '일정'">
+              <p>{{ task.from }}님이 회원님을 추가하였습니다.</p>
+              <p>{{ task.council }}{{ task.content }}({{ task.time }})</p>
+              <p class="card-content-announce">
+                {{ task.type }} <span>| {{ '오늘' }}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <article class="alarm__footer">
+          <div class="alarm__footer-announce">
+            ※ 알림 목록은 최대 20개 까지 7일간 표시됩니다.
+          </div>
+        </article>
+      </article>
+    </article>
+  </section>
 </template>
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      schedules: [
+        {
+          date: '2022년 7월 14일',
+          body: [
+            {
+              type: '친구',
+              from: '이필웅',
+              read: true,
+              porfileUrl: require('../../assets/img_flitto.png'),
+            },
+            {
+              type: '일정',
+              from: '김효은',
+              council: '[아고라]',
+              content: '토이 프로젝트 미팅',
+              time: '오후 12:00 - 오후 13:00',
+              read: true,
+              porfileUrl: require('../../assets/img_flitto.png'),
+            },
+          ],
+        },
+        {
+          date: '2022년 7월 12일',
+          body: [
+            {
+              type: '친구',
+              from: '김도경',
+              read: false,
+              porfileUrl: require('../../assets/img_flitto.png'),
+            },
+            {
+              type: '일정',
+              from: '윤성철',
+              council: '[아고라]',
+              content: '토이 프로젝트 미팅',
+              time: '오후 14:00 - 오후 15:00',
+              read: false,
+              porfileUrl: require('../../assets/img_flitto.png'),
+            },
+            {
+              type: '일정',
+              from: '이필웅',
+              council: '[독도]',
+              content: '토이 프로젝트 미팅',
+              time: '오후 15:00 - 오후 16:00',
+              read: false,
+              porfileUrl: require('../../assets/img_flitto.png'),
+            },
+          ],
+        },
+      ],
+    }
+  },
+}
 </script>
 <style lang="scss" scoped>
-.test {
-  border: 3px solid gold;
-  width: 300px;
-  height: 300px;
-  background: inherit;
+@mixin schedule-card($card, $alarm) {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 110px;
+  border-bottom: 1px solid var(--color-gray);
+  background: var($card);
 
-  h1 {
-    background: inherit;
+  &:last-child {
+    margin-bottom: 2.5em;
+  }
+
+  .alarm__body-card-icon {
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    margin: 0 1.5em 0 0.3em;
+    background: var($alarm);
+  }
+}
+.alarm {
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+
+  .alarm__header {
+    .alarm__header-title {
+      margin: 1em 0 3.5em 1.5em;
+      font-size: var(--font-size-h4);
+      font-weight: bold;
+      color: var(--color-blue);
+    }
+
+    .alarm__header-menu {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+
+      .alarm__header-menubox {
+        margin-left: 4.5em;
+        font-size: var(--font-size-h4);
+        color: var(--color-gray);
+
+        .menubox-all {
+          margin-right: 0.5em;
+          cursor: pointer;
+        }
+        .menubox-friend {
+          margin: 0 0.5em 0 0.5em;
+          cursor: pointer;
+        }
+
+        .menubox-schedule {
+          margin-left: 0.5em;
+          cursor: pointer;
+        }
+      }
+
+      .menubox-read-all {
+        margin-right: 3em;
+        font-size: var(--font-size-h4);
+        color: var(--color-blue);
+        text-decoration: underline;
+        text-underline-position: under;
+        cursor: pointer;
+      }
+    }
+  }
+  .alarm__body {
+    display: flex;
+    flex-direction: column;
+    margin: 2em 3.6em 0 5.6em;
+    justify-content: center;
+
+    .alarm__body-date {
+      position: relative;
+      bottom: 1em;
+      display: flex;
+      margin: auto;
+      justify-content: center;
+      align-items: center;
+      background: var(--color-blue);
+      color: var(--color-white);
+      border-radius: 10px;
+      width: 150px;
+      height: 30px;
+    }
+
+    .alarm__body-card-box {
+      @include schedule-card(--color-light-blue, --color-blue);
+    }
+
+    .alarm__body-card-box-clicked {
+      @include schedule-card(--color-light-gray, --color-gray);
+    }
+    .alarm__body-card-porifle {
+      width: 35px;
+      height: 35px;
+      border-radius: 50%;
+      margin-right: 2.5em;
+    }
+
+    .alarm__body-card-content {
+      .card-content-announce {
+        font-weight: bold;
+        margin-top: 1.5em;
+
+        span {
+          font-weight: normal;
+          color: var(--color-gray);
+        }
+      }
+    }
+  }
+
+  .alarm__footer {
+    padding: 0 0 2em 0;
   }
 }
 </style>
