2786084b23e8559033a3604294e0c99caabc27f3
Leepilung <ek3434@naver.com>
2022-08-17 16:37:40 +0900

    refactor: 리뷰 사항 반영하여 코드 수정

--- src/components/alarm/MainAlaram.vue
+++ src/components/alarm/MainAlaram.vue
@@ -22,7 +22,7 @@
           <!-- 카드 부분 -->
           <div
             class="alarm__body-card-box"
-            :class="{ 'alarm__body-card-box-clicked': !task.read }"
+            :class="{ 'alarm__body-card-box--clicked': !task.read }"
             v-for="(task, i) in schedule.body"
             :key="i"
             @click="clickItem(task)"
@@ -34,7 +34,7 @@
             <div class="alarm__body-card-content" v-if="task.type === '친구'">
               <p>{{ task.from }}님이 회원님을 친구 추가 하였습니다.</p>
               <p class="card-content-announce">
-                {{ task.type }} <span>| {{ date }}</span>
+                {{ task.type }} <span>| {{ alarmOccurDate }}</span>
               </p>
             </div>
             <!-- 일정 추가일 경우 -->
@@ -42,7 +42,7 @@
               <p>{{ task.from }}님이 회원님을 추가하였습니다.</p>
               <p>{{ task.council }}{{ task.content }}({{ task.time }})</p>
               <p class="card-content-announce">
-                {{ task.type }} <span>| {{ date }}</span>
+                {{ task.type }} <span>| {{ alarmOccurDate }}</span>
               </p>
             </div>
           </div>
@@ -111,7 +111,7 @@ export default {
           ],
         },
       ],
-      date: '오늘',
+      alarmOccurDate: '오늘',
       menu: 'all',
     }
   },
@@ -158,103 +158,97 @@ export default {
   display: flex;
   flex-direction: column;
   overflow-y: auto;
+}
+.alarm__header-title {
+  margin: 1em 0 3.5em 1.5em;
+  font-size: var(--font-size-h4);
+  font-weight: bold;
+  color: var(--color-blue);
+}
 
-  .alarm__header {
-    .alarm__header-title {
-      margin: 1em 0 3.5em 1.5em;
-      font-size: var(--font-size-h4);
-      font-weight: bold;
+.alarm__header-menu {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  .alarm__header-menubox {
+    margin-left: 4.5em;
+    font-size: var(--font-size-h4);
+    color: var(--color-gray);
+
+    .menubox-all {
+      margin-right: 0.5em;
+      cursor: pointer;
+    }
+    .menubox-friend {
+      margin: 0 0.5em 0 0.5em;
+      cursor: pointer;
+    }
+
+    .menubox-schedule {
+      margin-left: 0.5em;
+      cursor: pointer;
+    }
+    .clicked {
       color: var(--color-blue);
-    }
-
-    .alarm__header-menu {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-
-      .alarm__header-menubox {
-        margin-left: 4.5em;
-        font-size: var(--font-size-h4);
-        color: var(--color-gray);
-
-        .menubox-all {
-          margin-right: 0.5em;
-          cursor: pointer;
-        }
-        .menubox-friend {
-          margin: 0 0.5em 0 0.5em;
-          cursor: pointer;
-        }
-
-        .menubox-schedule {
-          margin-left: 0.5em;
-          cursor: pointer;
-        }
-        .clicked {
-          color: var(--color-blue);
-          cursor: pointer;
-        }
-      }
-
-      .menubox-read-all {
-        margin-right: 3em;
-        font-size: var(--font-size-h4);
-        color: var(--color-blue);
-        text-decoration: underline;
-        text-underline-position: under;
-        cursor: pointer;
-      }
+      cursor: pointer;
     }
   }
-  .alarm__body {
+
+  .menubox-read-all {
+    margin-right: 3em;
+    font-size: var(--font-size-h4);
+    color: var(--color-blue);
+    text-decoration: underline;
+    text-underline-position: under;
+    cursor: pointer;
+  }
+}
+.alarm__body {
+  display: flex;
+  flex-direction: column;
+  margin: 2em 3.6em 0 5.6em;
+  justify-content: center;
+
+  .alarm__body-date {
+    position: relative;
+    bottom: 1em;
     display: flex;
-    flex-direction: column;
-    margin: 2em 3.6em 0 5.6em;
+    margin: auto;
     justify-content: center;
-
-    .alarm__body-date {
-      position: relative;
-      bottom: 1em;
-      display: flex;
-      margin: auto;
-      justify-content: center;
-      align-items: center;
-      background: var(--color-blue);
-      color: var(--color-white);
-      border-radius: 10px;
-      width: 150px;
-      height: 30px;
-    }
-
-    .alarm__body-card-box {
-      @include schedule-card(--color-light-blue, --color-blue);
-    }
-
-    .alarm__body-card-box-clicked {
-      @include schedule-card(--color-light-gray, --color-gray);
-    }
-    .alarm__body-card-porifle {
-      width: 35px;
-      height: 35px;
-      border-radius: 50%;
-      margin-right: 2.5em;
-    }
-
-    .alarm__body-card-content {
-      .card-content-announce {
-        font-weight: bold;
-        margin-top: 1.5em;
-
-        span {
-          font-weight: normal;
-          color: var(--color-gray);
-        }
-      }
-    }
+    align-items: center;
+    background: var(--color-blue);
+    color: var(--color-white);
+    border-radius: 10px;
+    width: 150px;
+    height: 30px;
   }
 
-  .alarm__footer {
-    padding: 0 0 2em 0;
+  .alarm__body-card-box {
+    @include schedule-card(--color-light-blue, --color-blue);
   }
+
+  .alarm__body-card-box--clicked {
+    @include schedule-card(--color-light-gray, --color-gray);
+  }
+  .alarm__body-card-porifle {
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    margin-right: 2.5em;
+  }
+}
+.card-content-announce {
+  font-weight: bold;
+  margin-top: 1.5em;
+
+  span {
+    font-weight: normal;
+    color: var(--color-gray);
+  }
+}
+
+.alarm__footer {
+  padding: 0 0 2em 0;
 }
 </style>
